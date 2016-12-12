2404815a88bd2c88310505277b3f1ee93cee8d24
Dimitris Xenakis <sparkdevelopment.social@gmail.com>
2016-12-12 05:30:16 +0100

    Base Files

--- SpatialDecision/resources.py
+++ SpatialDecision/resources.py
@@ -2,7 +2,8 @@
 
 # Resource object code
 #
-# Created by: The Resource Compiler for PyQt4 (Qt v4.8.6)
+# Created: Δευ 12. Δεκ 01:29:45 2016
+#      by: The Resource Compiler for PyQt (Qt v4.8.5)
 #
 # WARNING! All changes made in this file will be lost!
 
